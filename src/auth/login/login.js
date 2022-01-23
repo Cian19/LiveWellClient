29fde33fae5b35e5235f424ef6404716c83009c7
Cian Hutt <cswm.hutt1@gmail.com>
2022-01-23 18:26:36 +0000

    fix all bugs

--- src/auth/login/login.js
+++ src/auth/login/login.js
@@ -65,7 +65,7 @@ const Login = () => {
           id: res.data.id,
           status: true,
         });
-        console.log(authState.username);
+        console.log(username);
         history.push("/");
       }
     });
