9f39cd220aa5f592ee065dd060c13e3a85ed5f04
Cian Hutt <cswm.hutt1@gmail.com>
2022-01-23 18:21:42 +0000

    fix all bugs

--- src/auth/login/login.js
+++ src/auth/login/login.js
@@ -7,7 +7,7 @@ import { useHistory } from 'react-router';
 import { AuthContext } from '../../helpers/AuthContext';
 
 const Login = () => {
-  console.log(username);
+  console.log();
 
     const [username, setname] = useState("");
     const [password, setPassword] = useState("");
